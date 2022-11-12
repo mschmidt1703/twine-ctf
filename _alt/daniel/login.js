874539570ee5984ac05f830d7822eca2a517b549
daha1010 <d.hahn199gmail.com>
2022-11-13 00:06:40 +0100

    login js

---
+++ _alt/daniel/login.js
@@ -0,0 +1,11 @@
+function checkPassword(username, password)
+{
+  if( username === 'admin' && password === 'strongPassword098765' )
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
